e569f6fa564ad5138b0ed3beefd0ef0ea6d9070d
hieulenovo <hieubait99@gmail.com>
2024-04-04 09:46:06 +0700

    implement generator

---
+++ model/generator.py
@@ -0,0 +1,51 @@
+import torch.nn as nn
+from model.components import *
+
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+        self.encoder = nn.Sequential(
+            ConvBlock(in_channels=3, out_channels=64),
+            ConvBlock(in_channels=64, out_channels=128),
+            DownConv(channels=128),
+            ConvBlock(in_channels=128, out_channels=128),
+            SeparableConv2D(in_channels=128, out_channels=256),
+            DownConv(channels=256),
+            ConvBlock(in_channels=256, out_channels=256)
+        )
+
+        self.ir_blocks = nn.Sequential(
+            InvertedResblock(channels=256, out_channels=256),
+            InvertedResblock(channels=256, out_channels=256),
+            InvertedResblock(channels=256, out_channels=256),
+            InvertedResblock(channels=256, out_channels=256),
+            InvertedResblock(channels=256, out_channels=256),
+            InvertedResblock(channels=256, out_channels=256),
+            InvertedResblock(channels=256, out_channels=256),
+            InvertedResblock(channels=256, out_channels=256),
+        )
+
+        self.decoder = nn.Sequential(
+            ConvBlock(in_channels=256, out_channels=128),
+            UpConv(channels=128),
+            SeparableConv2D(128, 128),
+            ConvBlock(in_channels=128, out_channels=64),
+            UpConv(channels=64),
+            ConvBlock(in_channels=64, out_channels=64),
+            ConvBlock(in_channels=64, out_channels=64),
+            nn.Conv2d(64, 3, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.Tanh()
+        )
+    
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.ir_blocks(x)
+        x = self.decoder(x)
+        return x
+    
+# if __name__ == '__main__':
+#     import torch
+#     test_g_input = torch.randn(1, 3, 256, 256)
+#     G = Generator()
+#     test_g_output = G(test_g_input)
+#     print(test_g_output.shape)
